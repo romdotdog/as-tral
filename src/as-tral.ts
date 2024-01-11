a27c05efbe1c16e59cd913129cfded560eb3ff0a
romdotdog <70765447+romdotdog@users.noreply.github.com>
2024-01-10 18:28:01 -0600

    reverse suite results

--- src/as-tral.ts
+++ src/as-tral.ts
@@ -186,7 +186,7 @@ async function benchWASM(info: Info, binary: Uint8Array) {
                 const sigThresh = info.significanceLevel;
 
                 // sort suite results by time
-                currentSuiteResults.sort((a, b) => b[2] - a[2]);
+                currentSuiteResults.sort((a, b) => a[2] - b[2]);
 
                 console.log();
                 console.log(`Suite ${currentSuite} finished`);
